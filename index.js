1cd52e1fdeefbfd08e65cc40413ff3b969a57b5c
yunusga <yunusga@yandex.ru>
2017-10-23 17:20:10 +0500

    Plugin code

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-var postcss = require('postcss');
+const postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-momentum-scrolling', function (opts) {
-    opts = opts || {};
+module.exports = postcss.plugin('postcss-momentum-scrolling', (opts) => {
 
-    // Work with options here
+    opts = opts || {
+        short: false // true
+    };
 
-    return function (root, result) {
+    return function (root) {
 
-        // Transform CSS AST here
+        let selectors = [];
 
+        root.walkRules(rule => {
+
+            let hasMomentumScroll = false,
+                hasOverflow = false;
+
+            rule.walkDecls(decl => {
+
+                if (!hasMomentumScroll &&
+                    decl.prop === '-webkit-overflow-scrolling' &&
+                    decl.value === 'touch'
+                ) {
+                    hasMomentumScroll = true;
+                }
+
+                if (!hasOverflow &&
+                    decl.prop === 'overflow' &&
+                    decl.value !== 'hidden'
+                ) {
+                    hasOverflow = true;
+                }
+            });
+
+            if (!hasMomentumScroll && hasOverflow) {
+
+                selectors.push(rule.selector);
+
+                if (!opts.short) {
+
+                    rule.append({
+                        prop: '-webkit-overflow-scrolling',
+                        value: 'touch'
+                    });
+                }
+            }
+        });
+
+        if (selectors.length && opts.short) {
+
+            let selectorsUniq = selectors.filter((selector, index, arr) => {
+                return arr.indexOf(selector) === index;
+            });
+
+            let momentumRule = postcss.rule({
+                selector: selectorsUniq.join(',')
+            });
+
+            momentumRule.append({
+                prop: '-webkit-overflow-scrolling',
+                value: 'touch'
+            });
+
+            root.append(momentumRule);
+        }
     };
 });
