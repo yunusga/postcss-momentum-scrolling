42add8e9da23c48a6583e31c3f3412ffc74c15c4
Yunus Gaziev <yunus.gaziev@gmail.com>
2018-04-19 21:14:55 +0500

    added tests for new functionality

--- index.test.js
+++ index.test.js
@@ -2,8 +2,8 @@ const fs      = require('fs');
 const postcss = require('postcss');
 const plugin  = require('./');
 
-function hasMomentumScroll(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
+function testEqualResult(input, output, opts) {
+    return postcss([ plugin(opts) ]).process(input, { from: undefined })
         .then(result => {
             expect(result.css).toEqual(output);
             expect(result.warnings().length).toBe(0);
@@ -15,5 +15,16 @@ it('if has momentum scroll, do nothing', () => {
     let input  = fs.readFileSync('./test/has-momentum.in.css', 'utf8');
     let output = fs.readFileSync('./test/has-momentum.out.css', 'utf8');
 
-    return hasMomentumScroll(input, output, {});
+    return testEqualResult(input, output);
+});
+
+it('custom options (hidden, inherit)', () => {
+
+    let input  = fs.readFileSync('./test/custom-opts.in.css', 'utf8');
+    let output = fs.readFileSync('./test/custom-opts.out.css', 'utf8');
+
+    return testEqualResult(input, output, [
+        'hidden',
+        'inherit'
+    ]);
 });
