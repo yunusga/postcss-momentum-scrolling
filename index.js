7be6ae76b2885547a5972cd79403121359c61e89
Yunus Gaziev <yunusga@yandex.ru>
2018-12-03 00:18:32 +0500

    Fix eslint errors

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
-var postcss = require('postcss');
+var postcss = require('postcss')
 
 module.exports = postcss.plugin('postcss-momentum-scrolling', function (opts) {
+  opts = opts || [
+    'hidden',
+    'scroll',
+    'auto',
+    'inherit'
+  ]
 
-    opts = opts || [
-        'hidden',
-        'scroll',
-        'auto',
-        'inherit'
-    ];
+  return function (root) {
+    var hasMomentumScroll = false
 
-    return function (root) {
+    var hasOverflow = false
 
-        var hasMomentumScroll = false,
-            hasOverflow = false;
+    root.walkRules(function (rule) {
+      hasMomentumScroll = false
+      hasOverflow = false
 
-        root.walkRules(function (rule) {
+      rule.walkDecls(function (decl) {
+        if (!hasMomentumScroll &&
+          decl.prop === '-webkit-overflow-scrolling' &&
+          decl.value === 'touch'
+        ) {
+          hasMomentumScroll = true
+        }
 
-            hasMomentumScroll = false;
-            hasOverflow = false;
+        if (!hasOverflow &&
+          decl.prop.match(/^overflow/g) &&
+          opts.indexOf(decl.value) !== -1
+        ) {
+          hasOverflow = true
+        }
+      })
 
-            rule.walkDecls(function (decl) {
-
-                if (!hasMomentumScroll &&
-                    decl.prop === '-webkit-overflow-scrolling' &&
-                    decl.value === 'touch'
-                ) {
-                    hasMomentumScroll = true;
-                }
-
-                if (!hasOverflow &&
-                    decl.prop.match(/^overflow/g) &&
-                    opts.includes(decl.value)
-                ) {
-                    hasOverflow = true;
-                }
-            });
-
-            if (!hasMomentumScroll && hasOverflow) {
-
-                rule.append({
-                    prop: '-webkit-overflow-scrolling',
-                    value: 'touch'
-                });
-            }
-        });
-    };
-});
+      if (!hasMomentumScroll && hasOverflow) {
+        rule.append({
+          prop: '-webkit-overflow-scrolling',
+          value: 'touch'
+        })
+      }
+    })
+  }
+})
--- index.test.js
+++ index.test.js
@@ -1,30 +1,28 @@
-const fs      = require('fs');
-const postcss = require('postcss');
-const plugin  = require('./');
+var fs = require('fs')
+var postcss = require('postcss')
+var plugin = require('./')
 
-function testEqualResult(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input, { from: undefined })
-        .then(result => {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
+function testEqualResult (input, output, opts) {
+  return postcss([plugin(opts)]).process(input, { from: undefined })
+    .then(function (result) {
+      expect(result.css).toEqual(output)
+      expect(result.warnings()).toHaveLength(0)
+    })
 }
 
-it('if has momentum scroll, do nothing', () => {
+it('if has momentum scroll, do nothing', function () {
+  var input = fs.readFileSync('./test/has-momentum.in.css', 'utf8')
+  var output = fs.readFileSync('./test/has-momentum.out.css', 'utf8')
 
-    let input  = fs.readFileSync('./test/has-momentum.in.css', 'utf8');
-    let output = fs.readFileSync('./test/has-momentum.out.css', 'utf8');
+  return testEqualResult(input, output)
+})
 
-    return testEqualResult(input, output);
-});
+it('custom options (hidden, inherit)', function () {
+  var input = fs.readFileSync('./test/custom-opts.in.css', 'utf8')
+  var output = fs.readFileSync('./test/custom-opts.out.css', 'utf8')
 
-it('custom options (hidden, inherit)', () => {
-
-    let input  = fs.readFileSync('./test/custom-opts.in.css', 'utf8');
-    let output = fs.readFileSync('./test/custom-opts.out.css', 'utf8');
-
-    return testEqualResult(input, output, [
-        'hidden',
-        'inherit'
-    ]);
-});
+  return testEqualResult(input, output, [
+    'hidden',
+    'inherit'
+  ])
+})
