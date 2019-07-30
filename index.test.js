cf6fa09e24590d62ace609d9dab080c0f0c49a3a
Yunus Gaziev <yunusga@yandex.ru>
2019-07-30 16:45:53 +0500

    ESLint fixes

--- index.test.js
+++ index.test.js
@@ -1,26 +1,26 @@
-var fs = require('fs')
-var postcss = require('postcss')
+let fs = require('fs')
+let postcss = require('postcss')
 
-var plugin = require('./')
+let plugin = require('./')
 
 function testEqualResult (input, output, opts) {
   return postcss([plugin(opts)]).process(input, { from: undefined })
-    .then(function (result) {
+    .then(result => {
       expect(result.css).toEqual(output)
       expect(result.warnings()).toHaveLength(0)
     })
 }
 
-it('if has momentum scroll, do nothing', function () {
-  var input = fs.readFileSync('./test/has-momentum.in.css', 'utf8')
-  var output = fs.readFileSync('./test/has-momentum.out.css', 'utf8')
+it('if has momentum scroll, do nothing', () => {
+  let input = fs.readFileSync('./test/has-momentum.in.css', 'utf8')
+  let output = fs.readFileSync('./test/has-momentum.out.css', 'utf8')
 
   return testEqualResult(input, output)
 })
 
-it('custom options (hidden, inherit)', function () {
-  var input = fs.readFileSync('./test/custom-opts.in.css', 'utf8')
-  var output = fs.readFileSync('./test/custom-opts.out.css', 'utf8')
+it('custom options (hidden, inherit)', () => {
+  let input = fs.readFileSync('./test/custom-opts.in.css', 'utf8')
+  let output = fs.readFileSync('./test/custom-opts.out.css', 'utf8')
 
   return testEqualResult(input, output, [
     'hidden',
